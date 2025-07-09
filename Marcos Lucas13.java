ef75b91c4851337012a0e3b3c5dec2d44703dde6
F1R3Z4P <118454658+F1reZap@users.noreply.github.com>
2025-07-09 11:51:13 -0300

    Update Marcos Lucas13.java

--- Marcos Lucas13.java
+++ Marcos Lucas13.java
@@ -2,21 +2,29 @@ import java.util.Scanner;
 
 public class lista4 {
     public static void main(String[] args) {
-        int mumero = 0, veri1;
+        int num = 0, o=2, imp=1;
         Scanner teclado = new Scanner(System.in);
-        System.out.println("digete um numero: ");
-        mumero = teclado.nextInt();
-        veri1=mumero%2;
-        if (mumero!=2){
-            if (veri1==0){
-                System.out.println("Não é primo");
-                System.out.println(veri1);
+        System.out.println("numero: ");
+        num = teclado.nextInt();
+        double verif1=0;
+        boolean primo = false;
+        while (o<=num){
+            verif1 =num%o;
+            if (verif1==0){
+                primo = false;
             } else {
-                System.out.println(mumero+" é Primo");
+                primo = true;
             }
-        } else {
-            System.out.println("2 é primo");
+            o++;
         }
+        if (num == 2){
+            System.out.println(num+". é primo");
+        } else if (verif1 != 0) {
+            System.out.println(num+". é primo");
+        } else {
+            System.out.println(num+". não é primo");
+        }
+
 
         teclado.close();
     }
