cc36476eb6c79a0d58060b71ecc46f45db0bfdd9
F1R3Z4P <118454658+F1reZap@users.noreply.github.com>
2025-07-10 07:43:05 -0300

    Update Marcos Lucas13.java

--- Marcos Lucas13.java
+++ Marcos Lucas13.java
@@ -1,53 +1,34 @@
 import java.util.Scanner;
 
 public class lista4 {
-    public static boolean isPrimo(int numero) {
-        if (numero <= 1) {
-            return false;
-        }
-        for (int i = 2; i * i <= numero; i++) {
-            if (numero % i == 0) {
-                return false;
+    public static void main(String[] args) {
+        int num = 0, o=2, imp=1;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("numero: ");
+        num = teclado.nextInt();
+        double veriF1 = Math.sqrt(num), veriF2=0;
+        if (num<=0){
+            System.out.println("numero não pode ser primo");
+        } else {
+            if (num!=2){
+                if (num %2==0){
+                    System.out.println(num+". não é primo");
+                } else {
+                    while (imp<=veriF1){
+                        imp=imp+2;
+                        veriF2 = num%imp;
+                        if (veriF2==0){
+                            System.out.println(num+". não é primo");
+                            return;
+                        }
+                    }
+                    System.out.println(num+". é primo");
+                }
+
+            } else {
+                System.out.println(num+". é primo");
             }
         }
-        return true;
+        teclado.close();
     }
-
-    public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-
-        int numero = teclado.nextInt();
-        if (isPrimo(numero)) {
-            System.out.println(numero + " é primo");
-        } else {
-            System.out.println(numero + " não é primo");
-        }
-    }
-//    public static void main(String[] args) {
-//        int num = 0, o=2, imp=1;
-//        Scanner teclado = new Scanner(System.in);
-//        System.out.println("numero: ");
-//        num = teclado.nextInt();
-//        double verif1=0;
-//        boolean primo = false;
-//        while (o<=num){
-//            verif1 =num%o;
-//            if (verif1==0){
-//                primo = false;
-//            } else {
-//                primo = true;
-//            }
-//            o++;
-//        }
-//        if (num == 2){
-//            System.out.println(num+". é primo");
-//        } else if (verif1 != 0) {
-//            System.out.println(num+". é primo");
-//        } else {
-//            System.out.println(num+". não é primo");
-//        }
-//
-//
-//        teclado.close();
-//    }
 }
