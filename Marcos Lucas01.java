7decc105b9a89d614acca57139a6a41551a719b5
F1R3Z4P <118454658+F1reZap@users.noreply.github.com>
2025-07-24 08:15:56 -0300

    Add files via upload

--- Marcos Lucas01.java
+++ Marcos Lucas01.java
@@ -1,42 +1,42 @@
 import java.util.Scanner;
-
 public class lista5 {
     public static void main(String[] args){
-        int Tmai=0, Tmen=0, AMN=0, AMV=0, ID=0;
+        int Tmai=0, Tmen=0, AMN=0, AMV=0;
+        int ID[] = new int[40];
         double Media=0, medG=0;
         Scanner teclado = new Scanner(System.in);
-        for (int i = 1; i<=40; i++){
-            System.out.println("Digite a idade do aluno: ");
-            ID = teclado.nextInt();
-            if (i==1){
-                AMN = ID;
-                AMV = ID;
-                if (ID<=16){
+        for (int i = 0; i<40; i++){
+            System.out.println("Digite o aluno "+i);
+            ID[i] = teclado.nextInt();
+            if (i==0){
+                AMN = ID[i];
+                AMV = ID[i];
+                if (ID[i]<=16){
                     Tmen++;
                 } else {
                     Tmai++;
                 }
-                Media = ID/i;
-                medG = Media;
             } else {
-                if (ID<=16){
+                if (ID[i]<AMN){
+                    AMN = ID[i];
+                } else if (ID[i]>AMV) {
+                    AMV = ID[i];
+                }
+                if (ID[i]<=16){
                     Tmen++;
                 } else {
                     Tmai++;
                 }
-                if (ID<AMN){
-                    AMN = ID;
-                } else if (ID>AMV) {
-                    AMV = ID;
-                }
-                Media = (medG+ID)/i;
-                medG = Media;
+
             }
+            Media += ID[i];
         }
-        System.out.println("Alunos com menos ou igual a 16: "+Tmen);
-        System.out.println("Alunos com maior a 16: "+Tmai);
+        medG = Media/40;
+        System.out.println("Alunos com idade menor ou igual a 16: "+Tmen);
+        System.out.println("Alunos com idade maior a 16: "+Tmai);
         System.out.println("Média das idades: "+medG);
-        System.out.println("Aluno mais novo: "+AMN);
-        System.out.println("Aluno mais velho: "+AMV);
+        System.out.println("Idade do aluno mais novo: "+AMN);
+        System.out.println("Idade do aluno mais velho: "+AMV);
+        teclado.close();
     }
 }
