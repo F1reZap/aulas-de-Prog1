0fd766a5f723c28eb07f7f165c45740cbebc48a7
F1R3Z4P <118454658+F1reZap@users.noreply.github.com>
2025-08-04 22:28:39 -0300

    Update Marcos Lucas12.java

--- Marcos Lucas12.java
+++ Marcos Lucas12.java
@@ -5,27 +5,24 @@ public class lista5 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         int Vet[] = new int[10];
+        boolean ordenado = true;
 
         for (int i = 0; i < 10; i++) {
             System.out.println("Digite o número na posição " + i + ":");
             Vet[i] = teclado.nextInt();
         }
-        System.out.println("Vetor original: " + Arrays.toString(Vet));
+        System.out.println(Arrays.toString(Vet));
 
-        boolean trocou;
-        for (int pass = 0; pass < Vet.length - 1; pass++) {
-            trocou = false;
-            for (int i = 0; i < Vet.length - 1 - pass; i++) {
-                if (Vet[i] > Vet[i + 1]) {
-                    int temp = Vet[i];
-                    Vet[i] = Vet[i + 1];
-                    Vet[i + 1] = temp;
-                    trocou = true;
-//                    System.out.println(Arrays.toString(Vet));
-                }
+        for (int i = 0; i < Vet.length - 1; i++) {
+            if (Vet[i] > Vet[i + 1]) {
+                ordenado = false;
+                break;
             }
-            if (!trocou) break;
         }
-        System.out.println("Vetor ordenado: " + Arrays.toString(Vet));
+        if (ordenado) {
+            System.out.println("O vetor está ordenado crescentemente.");
+        } else {
+            System.out.println("O vetor NÃO está ordenado crescentemente.");
+        }
     }
 }
